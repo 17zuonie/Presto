1027e215ce762bc5ce73ee1c5fa8d13da2fa558a
sudo0015 <sudobash@qq.com>
2026-01-22 22:52:11 +0800

    7.3.7

--- PrestoConfig.py
+++ PrestoConfig.py
@@ -16,211 +16,46 @@ class BufSize(Enum):
 
 
 class Config(QConfig):
-    AutoRun = ConfigItem(
-        "MainWindow",
-        "AutoRun",
-        True,
-        BoolValidator()
-    )
-    Notify = ConfigItem(
-        "MainWindow",
-        "Notify",
-        True,
-        BoolValidator()
-    )
-    IsSourceCloud = OptionsConfigItem(
-        "MainWindow",
-        "IsSourceCloud",
-        True,
-        BoolValidator()
-    )
-    ScanCycle = RangeConfigItem(
-        "MainWindow",
-        "ScanCycle",
-        10,
-        RangeValidator(1, 50)
-    )
-    ConcurrentProcess = ConfigItem(
-        "MainWindow",
-        "ConcurrentProcess",
-        3,
-        RangeValidator(1, 5)
-    )
-    BufSize = OptionsConfigItem(
-        "MainWindow",
-        "BufSize",
-        BufSize._256,
-        OptionsValidator(BufSize),
-        EnumSerializer(BufSize)
-    )
-    dpiScale = OptionsConfigItem(
-        "MainWindow",
-        "DpiScale",
-        "Auto",
-        OptionsValidator([1, 1.25, 1.5, 1.75, 2, "Auto"]),
-        restart=True
-    )
+    AutoRun = ConfigItem("MainWindow", "AutoRun", True, BoolValidator())
+    Notify = ConfigItem("MainWindow", "Notify", True, BoolValidator())
+    IsSourceCloud = OptionsConfigItem("MainWindow", "IsSourceCloud", True, BoolValidator())
+    ScanCycle = RangeConfigItem("MainWindow", "ScanCycle", 10, RangeValidator(1, 50))
+    ConcurrentProcess = ConfigItem("MainWindow", "ConcurrentProcess", 3, RangeValidator(1, 5))
+    BufSize = OptionsConfigItem("MainWindow", "BufSize", BufSize._256, OptionsValidator(BufSize),
+                                EnumSerializer(BufSize))
+    dpiScale = OptionsConfigItem("MainWindow", "DpiScale", "Auto", OptionsValidator([1, 1.25, 1.5, 1.75, 2, "Auto"]),
+                                 restart=True)
 
-    sourceFolder = ConfigItem(
-        "Folders",
-        "SourceFolder",
-        "",
-        FolderValidator()
-    )
-    yuwenFolder = ConfigItem(
-        "Folders",
-        "Yuwen",
-        "",
-        FolderValidator()
-    )
-    shuxueFolder = ConfigItem(
-        "Folders",
-        "Shuxue",
-        "",
-        FolderValidator()
-    )
-    yingyuFolder = ConfigItem(
-        "Folders",
-        "Yingyu",
-        "",
-        FolderValidator()
-    )
-    wuliFolder = ConfigItem(
-        "Folders",
-        "Wuli",
-        "",
-        FolderValidator()
-    )
-    huaxueFolder = ConfigItem(
-        "Folders",
-        "Huaxue",
-        "",
-        FolderValidator()
-    )
-    shengwuFolder = ConfigItem(
-        "Folders",
-        "Shengwu",
-        "",
-        FolderValidator()
-    )
-    zhengzhiFolder = ConfigItem(
-        "Folders",
-        "Zhengzhi",
-        "",
-        FolderValidator()
-    )
-    lishiFolder = ConfigItem(
-        "Folders",
-        "Lishi",
-        "",
-        FolderValidator()
-    )
-    diliFolder = ConfigItem(
-        "Folders",
-        "Dili",
-        "",
-        FolderValidator()
-    )
-    jishuFolder = ConfigItem(
-        "Folders",
-        "Jishu",
-        "",
-        FolderValidator()
-    )
-    ziliaoFolder = ConfigItem(
-        "Folders",
-        "Ziliao",
-        "",
-        FolderValidator()
-    )
+    sourceFolder = ConfigItem("Folders", "SourceFolder", "", FolderValidator())
+    yuwenFolder = ConfigItem("Folders", "Yuwen", "", FolderValidator())
+    shuxueFolder = ConfigItem("Folders", "Shuxue", "", FolderValidator())
+    yingyuFolder = ConfigItem("Folders", "Yingyu", "", FolderValidator())
+    wuliFolder = ConfigItem("Folders", "Wuli", "", FolderValidator())
+    huaxueFolder = ConfigItem("Folders", "Huaxue", "", FolderValidator())
+    shengwuFolder = ConfigItem("Folders", "Shengwu", "", FolderValidator())
+    zhengzhiFolder = ConfigItem("Folders", "Zhengzhi", "", FolderValidator())
+    lishiFolder = ConfigItem("Folders", "Lishi", "", FolderValidator())
+    diliFolder = ConfigItem("Folders", "Dili", "", FolderValidator())
+    jishuFolder = ConfigItem("Folders", "Jishu", "", FolderValidator())
+    ziliaoFolder = ConfigItem("Folders", "Ziliao", "", FolderValidator())
 
-    IsSkipEmptyDir = OptionsConfigItem(
-        "Filter",
-        "IsSkipEmptyDir",
-        False,
-        BoolValidator()
-    )
-    IsSizeFilter = OptionsConfigItem(
-        "Filter",
-        "IsSizeFilter",
-        False,
-        BoolValidator()
-    )
-    SizeFilterUnit = OptionsConfigItem(
-        "Filter",
-        "SizeFilterUnit",
-        "GB",
-        OptionsValidator(["KB", "MB", "GB"])
-    )
-    SizeFilterValue = ConfigItem(
-        "Filter",
-        "SizeFilterValue",
-        1,
-        RangeValidator(1, 1024)
-    )
-    IsTypeFilter = OptionsConfigItem(
-        "Filter",
-        "IsTypeFilter",
-        False,
-        BoolValidator()
-    )
-    TypeFilterMode = OptionsConfigItem(
-        "Filter",
-        "TypeFilterMode",
-        "Exclude",
-        OptionsValidator(["Exclude", "Include"])
-    )
-    IsDocument = OptionsConfigItem(
-        "Filter",
-        "IsDocument",
-        False,
-        BoolValidator()
-    )
-    IsPicture = OptionsConfigItem(
-        "Filter",
-        "IsPicture",
-        False,
-        BoolValidator()
-    )
-    IsAudio = OptionsConfigItem(
-        "Filter",
-        "IsAudio",
-        False,
-        BoolValidator()
-    )
-    IsVideo = OptionsConfigItem(
-        "Filter",
-        "IsVideo",
-        False,
-        BoolValidator()
-    )
-    IsApplication = OptionsConfigItem(
-        "Filter",
-        "IsApplication",
-        False,
-        BoolValidator()
-    )
-    IsZipFile = OptionsConfigItem(
-        "Filter",
-        "IsZipFile",
-        False,
-        BoolValidator()
-    )
-    IsCustomType = OptionsConfigItem(
-        "Filter",
-        "IsCustomType",
-        False,
-        BoolValidator()
-    )
-    CustomType = ConfigItem(
-        "Filter",
-        "CustomTpe",
-        ".db, .lnk",
-        ConfigValidator()
-    )
+    IsSkipEmptyDir = OptionsConfigItem("Filter", "IsSkipEmptyDir", False, BoolValidator())
+    IsSizeFilter = OptionsConfigItem("Filter", "IsSizeFilter", False, BoolValidator())
+    SizeFilterUnit = OptionsConfigItem("Filter", "SizeFilterUnit", "GB", OptionsValidator(["KB", "MB", "GB"]))
+    SizeFilterValue = ConfigItem("Filter", "SizeFilterValue", 1, RangeValidator(1, 1024))
+    IsTypeFilter = OptionsConfigItem("Filter", "IsTypeFilter", False, BoolValidator())
+    TypeFilterMode = OptionsConfigItem("Filter", "TypeFilterMode", "Exclude", OptionsValidator(["Exclude", "Include"]))
+    IsDocument = OptionsConfigItem("Filter", "IsDocument", False, BoolValidator())
+    IsPicture = OptionsConfigItem("Filter", "IsPicture", False, BoolValidator())
+    IsAudio = OptionsConfigItem("Filter", "IsAudio", False, BoolValidator())
+    IsVideo = OptionsConfigItem("Filter", "IsVideo", False, BoolValidator())
+    IsApplication = OptionsConfigItem("Filter", "IsApplication", False, BoolValidator())
+    IsZipFile = OptionsConfigItem("Filter", "IsZipFile", False, BoolValidator())
+    IsCustomType = OptionsConfigItem("Filter", "IsCustomType", False, BoolValidator())
+    CustomType = ConfigItem("Filter", "CustomTpe", ".db, .lnk", ConfigValidator())
 
 
-YEAR = "2025"
-VERSION = "7.3.6"
+YEAR = "2026"
+VERSION = "7.3.7"
 cfg = Config()
 qconfig.load(os.path.join(os.path.expanduser('~'), '.Presto', 'config', 'config.json'), cfg)
